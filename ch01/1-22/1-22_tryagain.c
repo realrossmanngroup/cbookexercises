2a08c6acda9fdc754b36c6fb3257f565a28c15ae
Louis Rossmann <louis@rossmanngroup.com>
2023-10-15 04:12:12 -0400

    continuing to try to solve 1-22, and failing

--- ch01/1-22/1-22_tryagain.c
+++ ch01/1-22/1-22_tryagain.c
@@ -10,9 +10,11 @@ int main()
     int endpretruncate;         /*end of line, go back to here, this stays on line 1 when word wrapped*/
     int line2nullterminatorpos; /*where the null terminator goes on line 2*/
     int c, i, j, k, l;          /*for for loop*/
-    char s[MAXLINE];          /*store what we typed*/
-    char line1array[MAXLINE]; /*store line 1*/
-    char line2array[MAXLINE]; /*store line 2*/
+    char s[MAXLINE];            /*store what we typed*/
+    char line1array[MAXLINE];   /*store line 1*/
+    char line2array[MAXLINE];   /*store line 2*/
+
+    howfarin = endposttruncate = endpretruncate = line2nullterminatorpos = c = i = j = k = l = 0;
 
     for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; i++) /* get text as long as it's smaller than MAXLINE*/
     {
@@ -30,6 +32,7 @@ int main()
                     if (s[i] == ' ' || s[i] == '\t')
                     {
                         endpretruncate = i;
+                        printf("line %d says that we set endpretruncate %d equal to i %d\n", __LINE__, endpretruncate, i);
                         s[endpretruncate + 1] = '\0'; /*add null terminator to the end of line 1*/
                         break;
                     }
@@ -37,14 +40,18 @@ int main()
             }
         }
     }
-    for (j = 0; j <= endpretruncate; j++)
+    for (j = 0; j <= endpretruncate + 1; j++)
     { /*add truncated line to array for line 1*/
         line1array[j] = s[j];
-        printf("line %d code, line1array[%d] is %c, s[%d] is %c\n", __LINE__, j, line1array[j], j, s[j]);
+        printf("line %d code, line1array[%d] is %c, s[%d] is %c, endpretruncate is %d, endposttruncate is %d\n", __LINE__, j, line1array[j], j, s[j], endpretruncate, endposttruncate);
     }
     printf("%s\n", line1array);
+    printf("I just printed line1array above!\n");
     for (l = 0, k = endpretruncate; l <= endposttruncate - endpretruncate, k <= endposttruncate; l++, k++)
     {
         line2array[l] = s[k];
+        printf("line %d code, line2array[%d] is %c, s[%d] is %c, endpretruncate is %d, endposttruncate is %d\n", __LINE__, l, line2array[l], k, s[k], endpretruncate, endposttruncate);
     }
+    line2array[(endposttruncate - endpretruncate) + 1] = '\0';
+    printf("%s\n", line2array);
 }
