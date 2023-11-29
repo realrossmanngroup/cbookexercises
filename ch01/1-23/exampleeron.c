441bb185fdb8aeeb2e280d7cb5a757a08b083cc5
Louis Rossmann <louis@rossmanngroup.com>
2023-11-29 17:02:12 -0600

    1-23_delete_v17.c works with single line comments, works with double line comments, works with nested comments, can detect whether or not the comment is inside a string, whether a string with single quotes, double quotes, escape characters. That is it!

--- ch01/1-23/exampleeron.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    
-    printf("this is a /* multiline comment */ inside a printf command");
-    printf("the comment inside the printf command above is not a comment because it's inside my printf!"); 
-}
