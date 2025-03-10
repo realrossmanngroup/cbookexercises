0834b7d2854d0cb0fbaf96a6d962e20843238c7f
louis <louis@rossmanngroup.com>
2025-03-09 20:43:02 -0500

    made 1-17 clean and removed unnecessary debug code

--- ch01/1-17/2025_print_over_80_chars.c
+++ ch01/1-17/2025_print_over_80_chars.c
@@ -3,25 +3,18 @@
 #define MAXLINE 1000 /* maximum input line length */
 
 int oldgetline(char line[], int maxline);
-// void copy(char to[], char from[]);
 
-/* print the longest input line */
 int main()
 {
     int len = 0;        /* current line length */
     char line[MAXLINE]; /* current input line */
 
-    printf("\nlen is %d debug line %d\n", len, __LINE__);
     while ((len = oldgetline(line, MAXLINE)) != EOF)
     {
         if (len > 80)
         {
-            printf("\n\nMUSTBEOVER80 %s\n\n", line);
+            printf("\n Your line is over 80 characters! Here it is:\n\n%s\n\n", line);
         }
-      /* debug
-        printf("\nlen is %d debug line %d\n", len, __LINE__); // debug
-        printf("\nwhile loop runs again, len is %d debug line %d\n", len, __LINE__);
-        */
     }
 
     return 0;
@@ -37,9 +30,7 @@ int oldgetline(char s[], int lim)
         if (i < (lim - 2))
         {
             s[i] = c;
-            /* debug
-            printf("\ns[i] = %c, i = %d, debug line %d\n", s[i], i, __LINE__); 
-            */
+
             if (c == '\n')
             {
                 s[i] = c;
@@ -52,24 +43,9 @@ int oldgetline(char s[], int lim)
         }
         else
 
-        { /* debug
-            printf("\nc is %c, i = %d, debug line %d\n", c, i, __LINE__);
-            */
-
+        {
             continue;
         }
 
     return i;
 }
-
-/*
-// copy: copy 'from' into 'to'; assume to is big enough
-void copy(char to[], char from[])
-{
-    int i;
-
-    i = 0;
-    while ((to[i] = from[i]) != '\0')
-        ++i;
-}
-*/
